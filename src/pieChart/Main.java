0873d7de4dec4ef8b0e203f222a43ac0462073a4
Creighton Lee <creighton.lee@hotmail.com>
2017-04-12 20:04:50 -0700

    FINAL CHANGES ADDED ICON and PIC

--- src/pieChart/Main.java
+++ src/pieChart/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +15,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pcap Shark App");
         primaryStage.setScene(new Scene(root, 1360, 700));
-
+        primaryStage.getIcons().add(new Image("pieChart/cuteshark.png"));
         primaryStage.show();
     }
 
